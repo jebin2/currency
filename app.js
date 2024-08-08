89aab54e8f7accfa845a5ab87e9d114ba764bca2
jebin2 <jebineinstein@gmail.com>
2024-08-09 00:01:06 +0530

    localstorage added.

--- app.js
+++ app.js
@@ -26,52 +26,63 @@ if ('serviceWorker' in navigator) {
 
         return convertedAmount;
     }
+    function processData() {
+        
+        const exchangeRateElement = document.getElementById('exchangeRate');
+        const fromSelect = document.getElementById('fromSelect');
+        const toSelect = document.getElementById('toSelect');
+        const fromAmount = document.getElementById('fromAmount');
+        const toAmount = document.getElementById('toAmount');
+        
+        const rates = data.rates;
+        loadingElement.style.display = 'none';
+
+        let currencies = Object.keys(rates);
+        let currencyOptions = currencies.map(cur => `<option value="${cur}">${cur}</option>`).join('');
+        fromSelect.innerHTML = currencyOptions;
+        fromSelect.value = "USD";
+        toSelect.innerHTML = currencyOptions;
+        toSelect.value = "INR";
+
+        updateSelectedValue(exchangeRateElement, fromSelect, toSelect, rates);
+
+        fromSelect.addEventListener('change', () => {
+            convertCurrency(1, fromSelect.value, toSelect.value, rates);
+            updateSelectedValue(exchangeRateElement, fromSelect, toSelect, rates);
+        });
+
+        toSelect.addEventListener('change', () => {
+            updateSelectedValue(exchangeRateElement, fromSelect, toSelect, rates);
+        });
+
+        fromAmount.addEventListener('input', (event) => {
+            toAmount.value = event.target.value * rates[toSelect.value];
+        });
+
+        toAmount.addEventListener('input', (event) => {
+            fromAmount.value =  event.target.value / rates[toSelect.value];
+        });
+    }
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/currency/service-worker.js')
             .then(registration => {
                 const loadingElement = document.getElementById('loading');
-                const exchangeRateElement = document.getElementById('exchangeRate');
-                const fromSelect = document.getElementById('fromSelect');
-                const toSelect = document.getElementById('toSelect');
-                const fromAmount = document.getElementById('fromAmount');
-                const toAmount = document.getElementById('toAmount');
 
-                fetch('https://jeapis.netlify.app/.netlify/functions/currency?from=USD&to=INR')
+                const currencyData = localStorage.getItem('currencyData');
+                if(currencyData) {
+                    processData(currencyData);
+                } else {
+                    fetch('https://jeapis.netlify.app/.netlify/functions/currency?from=USD&to=INR')
                     .then(response => response.json())
                     .then(data => {
-                        const rates = data.rates;
-                        loadingElement.style.display = 'none';
-
-                        let currencies = Object.keys(rates);
-                        let currencyOptions = currencies.map(cur => `<option value="${cur}">${cur}</option>`).join('');
-                        fromSelect.innerHTML = currencyOptions;
-                        fromSelect.value = "USD";
-                        toSelect.innerHTML = currencyOptions;
-                        toSelect.value = "INR";
-
-                        updateSelectedValue(exchangeRateElement, fromSelect, toSelect, rates);
-
-                        fromSelect.addEventListener('change', () => {
-                            convertCurrency(1, fromSelect.value, toSelect.value, rates);
-                            updateSelectedValue(exchangeRateElement, fromSelect, toSelect, rates);
-                        });
-
-                        toSelect.addEventListener('change', () => {
-                            updateSelectedValue(exchangeRateElement, fromSelect, toSelect, rates);
-                        });
-
-                        fromAmount.addEventListener('input', (event) => {
-                            toAmount.value = event.target.value * rates[toSelect.value];
-                        });
-
-                        toAmount.addEventListener('input', (event) => {
-                            fromAmount.value =  event.target.value / rates[toSelect.value];
-                        });
+                        localStorage.setItem('currencyData', JSON.stringify(data));
+                        processData(data);
                     })
                     .catch(error => {
                         console.error('Error fetching data:', error);
                         loadingElement.textContent = 'Failed to load data. Please try again later.';
                     });
+                }
             })
             .catch(error => {
                 console.log('ServiceWorker registration failed: ', error);
